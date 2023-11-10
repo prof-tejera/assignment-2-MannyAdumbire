7916352e648cb0b19dc85a4465c929ecdfae1ae3
MannyAdumbire <MannyAdumbire@users.noreply.github.com>
2023-11-10 01:17:18 +0000

    deploy: a203f82cd5a999b424ba2adb7256037963b7ac57

---
+++ precache-manifest.ef286973921a38e5c059a44fdf9396b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9cd0c391903b67c3eb8c7c52a5249d0",
+    "url": "/assignment-2-MannyAdumbire/index.html"
+  },
+  {
+    "revision": "bbe18fa2924b1b2b0ebe",
+    "url": "/assignment-2-MannyAdumbire/static/css/main.318c9d8e.chunk.css"
+  },
+  {
+    "revision": "09908cb86ca47a40d93b",
+    "url": "/assignment-2-MannyAdumbire/static/js/2.d63a0dc4.chunk.js"
+  },
+  {
+    "revision": "bbe18fa2924b1b2b0ebe",
+    "url": "/assignment-2-MannyAdumbire/static/js/main.2b4463a2.chunk.js"
+  },
+  {
+    "revision": "be30801409c402e97770",
+    "url": "/assignment-2-MannyAdumbire/static/js/runtime~main.e5355b34.js"
+  }
+]);
